f969451b798f4db228bf5c7c3f24050f32a6b86d
davidpypysp <davidpy1992@gmail.com>
2017-09-19 20:48:36 -0700

    update

--- softrd/src/math/vector.h
+++ softrd/src/math/vector.h
@@ -56,7 +56,7 @@ struct vec3 {
 	inline float operator * (const vec3 &b) const { return x*b.x + y*b.y + z*b.z; } // dot product
 	inline vec3 operator % (const vec3 &b) const { return vec3(y*b.z - z*b.y, z*b.x - x*b.z, x*b.y - y*b.x); } // cross product
 	inline vec3 operator / (const float b) const { return vec3(x / b, y / b, z / b); }
-	inline vec3& operator = (const vec3 &b) { x = b.x; y = b.y; z = b.z; return *this; }
+	    
 	inline vec3 operator - () const { return vec3(-x, -y, -z); }
 	inline float lengthSqr() { return x*x + y*y + z*z; }
 	inline float length() { return sqrt(x*x + y*y + z*z); }
