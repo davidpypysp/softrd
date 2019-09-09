cf5262d2f11de823fb036d3a697f3d9db14148dc
Jiyu Peng <jpeng@autox.ai>
2019-09-09 15:18:44 -0700

    add canvas; refactor css

--- softrd_gui/src/components/Layouts/MainView.js
+++ softrd_gui/src/components/Layouts/MainView.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default class MainView extends React.Component {
-  constructor(props) {
-      super(props);
-  }
-  
-  render() {
-      return (
-          <div className="main-view">
-              {"main view"}
-          </div>
-      );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="main-view">
+                <canvas id="scene-canvas" className="scene-canvas">
+                </canvas>
+            </div>
+        );
+    }
 }
