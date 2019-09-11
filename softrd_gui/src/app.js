d0d001d3552a3c0ee36686d8231aa7c421cff2b5
Jiyu Peng <jpeng@autox.ai>
2019-09-11 16:36:39 -0700

    fix warning

--- softrd_gui/src/app.js
+++ softrd_gui/src/app.js
@@ -2,10 +2,6 @@ import React from "react";
 import GUI from "components/GUI";
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return <GUI />;
     }
--- softrd_gui/src/components/Layouts/Header.js
+++ softrd_gui/src/components/Layouts/Header.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return <div className="header">{"Header"}</div>;
     }
