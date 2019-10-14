b386abcfdbc33151dfa621b3a5ec71b3cbe9449f
davidpy <davidpy1992@gmail.com>
2019-10-13 20:45:57 -0700

    RunExample

--- core/src/renderer.cc
+++ core/src/renderer.cc
@@ -3,41 +3,39 @@
 namespace softrd {
 
 // Renderer initialization, include each stage's initialization
-Renderer::Renderer(const int width, const int height) : 
-width_(width), 
-height_(height),
-screen_size_(width * height),
-frame_buffer_(screen_size_ * 4),
-depth_buffer_(screen_size_),
-primitve_assembler_(width, height, vertex_out_buffer_),
-rasterizer_(width, height, fragment_buffer_),
-per_sample_proccessor_(width, height, depth_buffer_),
-device_(100, 100, width, height),
-camera_((float)width / (float)height),
-polygon_mode_(Rasterizer::TRIANGLE_FILL) {
+Renderer::Renderer(const int width, const int height) : width_(width),
+                                                        height_(height),
+                                                        screen_size_(width * height),
+                                                        frame_buffer_(screen_size_ * 4),
+                                                        depth_buffer_(screen_size_),
+                                                        primitve_assembler_(width, height, vertex_out_buffer_),
+                                                        rasterizer_(width, height, fragment_buffer_),
+                                                        per_sample_proccessor_(width, height, depth_buffer_),
+                                                        device_(100, 100, width, height),
+                                                        camera_((float)width / (float)height),
+                                                        polygon_mode_(Rasterizer::TRIANGLE_FILL) {
     rasterizer_.SetCamera(&camera_);
     device_.Setup();
     last_time_ = steady_clock::now();
-	inputs_.push_back(new InputUnit3("Camera", &camera_.position));
-	inputs_.push_back(new InputUnit3("Camera Direction", &camera_.direction));
+    inputs_.push_back(new InputUnit3("Camera", &camera_.position));
+    inputs_.push_back(new InputUnit3("Camera Direction", &camera_.direction));
     input_index_ = 0;
 }
 
 // main loop of renderer
-void Renderer::Run() {
+void Renderer::RunExample() {
     LoadCoordinateAxis();
-    
 
     // define object
     Mesh object;
     object.LoadCube2();
     vec3 object_position = vec3(0.0, 0.0, 0.0);
     inputs_.push_back(new InputUnit3("Object", &object_position));
-	vec3 object2_position = vec3(3.0, 0.0, 0.0);
-	inputs_.push_back(new InputUnit3("Object2", &object2_position));
+    vec3 object2_position = vec3(3.0, 0.0, 0.0);
+    inputs_.push_back(new InputUnit3("Object2", &object2_position));
     Material object_material(vec3(1.0, 0.5, 0.31), vec3(1.0, 0.5, 0.31), vec3(0.5, 0.5, 0.5), 32.0);
     Texture *diffuse_texture = new Texture("resource/container2.png");
-	Texture *specular_texture = new Texture("resource/container2_specular.png");
+    Texture *specular_texture = new Texture("resource/container2_specular.png");
     TextureMaterial object_material2(diffuse_texture, specular_texture, 32.0);
 
     // define lamp light
@@ -46,37 +44,34 @@ void Renderer::Run() {
     Light light(vec3(0.0, 0.0, 3.0), vec3(0.2, 0.2, 0.2), vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0));
     inputs_.push_back(new InputUnit3("Light", &light.position));
 
+    // define directional light
+    DirLight dir_light(vec3(-0.2, -1.0, -0.3), vec3(0.05, 0.05, 0.05), vec3(0.4, 0.4, 0.4), vec3(0.5, 0.5, 0.5));
+    inputs_.push_back(new InputUnit3("DirLight Direction", &dir_light.direction));
 
-	// define directional light
-	DirLight dir_light(vec3(-0.2, -1.0, -0.3), vec3(0.05, 0.05, 0.05), vec3(0.4, 0.4, 0.4), vec3(0.5, 0.5, 0.5));
-	inputs_.push_back(new InputUnit3("DirLight Direction", &dir_light.direction));
+    // define lamp point light
+    Mesh point_light_lamp;
+    point_light_lamp.LoadCube();
+    PointLight point_light(vec3(-3.0, 0.0, 0.0), vec3(0.05, 0.05, 0.05), vec3(0.8, 0.8, 0.8), vec3(1.0, 1.0, 1.0), 1.0, 0.09, 0.032);
+    inputs_.push_back(new InputUnit3("PointLight", &point_light.position));
 
+    // define lamp spot light
+    Mesh spot_light_lamp;
+    spot_light_lamp.LoadCube();
+    SpotLight spot_light(vec3(3.0, 0.0, 0.0), vec3(-1.0, 0.0, 0.0), cos(Radians(12.5)), cos(Radians(17.5)), vec3(0.1, 0.1, 0.1), vec3(0.8, 0.8, 0.8), vec3(1.0, 1.0, 1.0), 1.0, 0.09, 0.032);
+    inputs_.push_back(new InputUnit3("SpotLight", &spot_light.position));
 
-	// define lamp point light
-	Mesh point_light_lamp;
-	point_light_lamp.LoadCube();
-	PointLight point_light(vec3(-3.0, 0.0, 0.0), vec3(0.05, 0.05, 0.05), vec3(0.8, 0.8, 0.8), vec3(1.0, 1.0, 1.0), 1.0, 0.09, 0.032);
-	inputs_.push_back(new InputUnit3("PointLight", &point_light.position));
-
-	// define lamp spot light
-	Mesh spot_light_lamp;
-	spot_light_lamp.LoadCube();
-	SpotLight spot_light(vec3(3.0, 0.0, 0.0), vec3(-1.0, 0.0, 0.0), cos(Radians(12.5)), cos(Radians(17.5)), vec3(0.1, 0.1, 0.1), vec3(0.8, 0.8, 0.8), vec3(1.0, 1.0, 1.0), 1.0, 0.09, 0.032);
-	inputs_.push_back(new InputUnit3("SpotLight", &spot_light.position));
-
-	// define shaders
+    // define shaders
     VertexShaderLight vertex_shader_light;
     FragmentShader fragment_shader;
     FragmentShaderLightFull fragment_shader_light(camera_.position, object_material);
-	fragment_shader_light.AddLight(&spot_light);
+    fragment_shader_light.AddLight(&spot_light);
     FragmentShaderLightTexture fragment_shader_light_texture(camera_.position, object_material2);
-	fragment_shader_light_texture.AddLight(&light);
-	//fragment_shader_light_texture.AddLight(&dir_light);
-	//fragment_shader_light_texture.AddLight(&point_light);
-	//fragment_shader_light_texture.AddLight(&spot_light);
+    fragment_shader_light_texture.AddLight(&light);
+    //fragment_shader_light_texture.AddLight(&dir_light);
+    //fragment_shader_light_texture.AddLight(&point_light);
+    //fragment_shader_light_texture.AddLight(&spot_light);
 
-
-    while (device_.Quit() == false) { // renderer main loop, implement rendering pipeline here
+    while (device_.Quit() == false) {  // renderer main loop, implement rendering pipeline here
         // frame setting
         SetFrame();
 
@@ -89,25 +84,22 @@ void Renderer::Run() {
         // draw cordinate system
         DrawCoordinateAxis();
 
-        
         // -------------------------------------------------------------------------------
 
-		// update camera spot light
-		//spot_light.position = camera_.position;
-		//spot_light.direction = -camera_.direction;
-		mat4 model_matrix;
+        // update camera spot light
+        //spot_light.position = camera_.position;
+        //spot_light.direction = -camera_.direction;
+        mat4 model_matrix;
 
-        
         // 1. cube object
         model_matrix.identify();
         model_matrix.translate(object_position);
         vertex_shader_light.model_ = model_matrix;
         vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
 
-		//DrawObject(object, vertex_shader_light, fragment_shader_light_texture, Rasterizer::TRIANGLE_FILL, DRAW_TRIANGLE);
-		DrawObject(object, vertex_shader_light, fragment_shader_light, Rasterizer::TRIANGLE_FILL, DRAW_TRIANGLE);
+        //DrawObject(object, vertex_shader_light, fragment_shader_light_texture, Rasterizer::TRIANGLE_FILL, DRAW_TRIANGLE);
+        DrawObject(object, vertex_shader_light, fragment_shader_light, Rasterizer::TRIANGLE_FILL, DRAW_TRIANGLE);
 
-        
         // 2. light lamp
         model_matrix.identify();
         model_matrix.scale(0.1, 0.1, 0.1);
@@ -115,38 +107,33 @@ void Renderer::Run() {
         vertex_shader_light.model_ = model_matrix;
         vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
 
-		DrawObject(light_lamp, vertex_shader_light, fragment_shader);
+        DrawObject(light_lamp, vertex_shader_light, fragment_shader);
 
-		
-		// 3. point light lamp
-		model_matrix.identify();
-		model_matrix.scale(0.1, 0.1, 0.1);
-		model_matrix.translate(point_light.position);
-		vertex_shader_light.model_ = model_matrix;
-		vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
+        // 3. point light lamp
+        model_matrix.identify();
+        model_matrix.scale(0.1, 0.1, 0.1);
+        model_matrix.translate(point_light.position);
+        vertex_shader_light.model_ = model_matrix;
+        vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
 
-		DrawObject(point_light_lamp, vertex_shader_light, fragment_shader);
+        DrawObject(point_light_lamp, vertex_shader_light, fragment_shader);
 
+        // 4. spot light lamp
+        model_matrix.identify();
+        model_matrix.scale(0.1, 0.1, 0.1);
+        model_matrix.translate(spot_light.position);
+        vertex_shader_light.model_ = model_matrix;
+        vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
 
-		// 4. spot light lamp
-		model_matrix.identify();
-		model_matrix.scale(0.1, 0.1, 0.1);
-		model_matrix.translate(spot_light.position);
-		vertex_shader_light.model_ = model_matrix;
-		vertex_shader_light.transform_ = camera_.projection * camera_.view * model_matrix;
-
-		DrawObject(spot_light_lamp, vertex_shader_light, fragment_shader);
+        DrawObject(spot_light_lamp, vertex_shader_light, fragment_shader);
 
         // -------------------------------------------------------------------------------
-        
-
 
         // draw everything in the device
         DrawFrame();
     }
 
     Clear();
-
 }
 
 // use appointed vertex shader and fragment shader for rendering
@@ -156,7 +143,7 @@ void Renderer::SetShader(VertexShader *vertex_shader, FragmentShader *fragment_s
 }
 
 // rendering pipeline program
-void Renderer::Draw(const DrawMode mode) { 
+void Renderer::Draw(const DrawMode mode) {
     //vertex shader stage
     vertex_out_buffer_.clear();
     VertexOut vertex_out;
@@ -170,7 +157,7 @@ void Renderer::Draw(const DrawMode mode) {
         for (int index = 0; index < element_buffer_.size() / 2; ++index) {
             LinePrimitive line;
             // primitive assemble stage
-            if (primitve_assembler_.AssembleLine(element_buffer_[index * 2], element_buffer_[index * 2 + 1], &line) == false) continue; 
+            if (primitve_assembler_.AssembleLine(element_buffer_[index * 2], element_buffer_[index * 2 + 1], &line) == false) continue;
 
             // rasterize stage
             rasterizer_.DrawLinePrimitive(line);
@@ -185,10 +172,8 @@ void Renderer::Draw(const DrawMode mode) {
                     SetDepth(fragment_shader_out.window_position.x, fragment_shader_out.window_position.y, fragment_shader_out.window_position.z);
                 }
             }
-
         }
-    }
-    else if (mode == DRAW_TRIANGLE) {
+    } else if (mode == DRAW_TRIANGLE) {
         for (int index = 0; index < element_buffer_.size() / 3; ++index) {
             std::vector<TrianglePrimitive> triangles;
             primitve_assembler_.AssembleTriangle(element_buffer_[index * 3], element_buffer_[index * 3 + 1], element_buffer_[index * 3 + 2], &triangles);
@@ -208,16 +193,14 @@ void Renderer::Draw(const DrawMode mode) {
             }
         }
     }
-
 }
-
 
 // Draw mesh using rendering pipeline
 void Renderer::DrawObject(Mesh &mesh, VertexShader &vertex_shader, FragmentShader &fragment_shader, const Rasterizer::DrawTriangleMode tri_mode, const DrawMode draw_mode) {
-	mesh.LoadBuffer(vertex_buffer_, element_buffer_);
-	SetShader(&vertex_shader, &fragment_shader);
-	SetPolygonMode(tri_mode);
-	Draw(draw_mode);
+    mesh.LoadBuffer(vertex_buffer_, element_buffer_);
+    SetShader(&vertex_shader, &fragment_shader);
+    SetPolygonMode(tri_mode);
+    Draw(draw_mode);
 }
 
 // set polygonmode in triangle mode or wireframe mode
@@ -228,7 +211,6 @@ void Renderer::SetPolygonMode(const Rasterizer::DrawTriangleMode mode) {
 void Renderer::ResetBuffer() {
     frame_buffer_.Fill(10);
     depth_buffer_.Fill(1.0);
-
 }
 
 void Renderer::Clear() {
@@ -241,10 +223,10 @@ Renderer::~Renderer() {
 void Renderer::SetPixel(const int x, const int y, const vec4 &color) {
     if (0 <= x && x <= width_ && 0 <= y && y <= height_) {
         int offset = (y * width_ + x) * 4;
-        frame_buffer_[offset] = Clamp(color.z * 255, 0, 255); // b
-        frame_buffer_[offset+1] = Clamp(color.y * 255, 0, 255); // g
-        frame_buffer_[offset+2] = Clamp(color.x * 255, 0, 255); // r
-        frame_buffer_[offset+3] = Clamp(color.w * 255, 0, 255); // a 
+        frame_buffer_[offset] = Clamp(color.z * 255, 0, 255);      // b
+        frame_buffer_[offset + 1] = Clamp(color.y * 255, 0, 255);  // g
+        frame_buffer_[offset + 2] = Clamp(color.x * 255, 0, 255);  // r
+        frame_buffer_[offset + 3] = Clamp(color.w * 255, 0, 255);  // a
     }
 }
 
@@ -283,8 +265,6 @@ void Renderer::DrawFrame() {
     device_.RenderPresent();
 }
 
-
-
 void Renderer::Input() {
     device_.HandleEvents();
 
@@ -292,8 +272,7 @@ void Renderer::Input() {
     if (device_.PressKeyR()) {
         input_index_ += kPressValue;
         if (input_index_ >= inputs_.size()) input_index_ = 0;
-    }
-    else {
+    } else {
         input_index_ = floor(input_index_);
     }
 
@@ -309,14 +288,11 @@ void Renderer::Input() {
     if (device_.PressKeyJ()) move.w -= move_step;
     if (device_.PressKeyL()) move.w += move_step;
 
-
-    if ((int)input_index_ == 0) { // move camera
+    if ((int)input_index_ == 0) {  // move camera
         camera_.Move(move.getVec3());
-    }
-    else { // move other position
+    } else {  // move other position
         inputs_[input_index_]->Move(move);
     }
-
 
     // rotate camera
     float degree = 1.5;
@@ -327,7 +303,6 @@ void Renderer::Input() {
     if (device_.PressKeyRight()) rotate.y += degree;
     camera_.Rotate(rotate);
 
-
     // fov setting
     if (device_.PressKeyQ()) camera_.Zoom(-degree);
     if (device_.PressKeyE()) camera_.Zoom(degree);
@@ -337,8 +312,6 @@ bool Renderer::Press() {
     return false;
 }
 
-
-
 void Renderer::LoadCoordinateAxis() {
     axis_lines_[0].LoadLine(vec3(0, 0, 0), vec3(10, 0, 0), vec3(1, 0, 0));
     axis_lines_[1].LoadLine(vec3(0, 0, 0), vec3(0, 10, 0), vec3(0, 1, 0));
@@ -346,7 +319,6 @@ void Renderer::LoadCoordinateAxis() {
 
     grid_line_x_.LoadLine(vec3(-10, 0, 0), vec3(10, 0, 0));
     grid_line_y_.LoadLine(vec3(0, 0, -10), vec3(0, 0, 10));
-
 }
 
 // Draw Coordinate Axis function
@@ -357,7 +329,7 @@ void Renderer::DrawCoordinateAxis() {
     FragmentShaderFlatColor fragment_shader;
 
     SetShader(&vertex_shader, &fragment_shader);
-    
+
     for (int i = 0; i < 3; ++i) {
         axis_lines_[i].LoadBuffer(vertex_buffer_, element_buffer_);
         fragment_shader.flat_color = axis_lines_[i].color;
@@ -383,5 +355,4 @@ void Renderer::DrawCoordinateAxis() {
     }
 }
 
-} // namespace softrd
-
+}  // namespace softrd
--- core/src/renderer.h
+++ core/src/renderer.h
@@ -1,21 +1,19 @@
 #ifndef SOFTRD_RENDERER_H_
 #define SOFTRD_RENDERER_H_
 
-#include <vector>
 #include <chrono>
-
+#include <vector>
 
 #include "src/components/camera.h"
-#include "src/modules/vertex_shader.h"
-#include "src/modules/rasterizer.h"
+#include "src/components/model.h"
+#include "src/devices/device.h"
+#include "src/inputs/input.h"
 #include "src/modules/fragment_shader.h"
 #include "src/modules/per_sample_processing.h"
-#include "src/devices/device.h"
-#include "src/components/model.h"
-#include "src/utils/util.h"
-#include "src/inputs/input.h"
+#include "src/modules/rasterizer.h"
 #include "src/modules/vertex_loader.h"
-
+#include "src/modules/vertex_shader.h"
+#include "src/utils/util.h"
 
 using namespace std::chrono;
 
@@ -24,81 +22,72 @@ namespace softrd {
 // Renderer class deals with the whole process of rendering pipeline
 // It start from reading .obj file to finally set the frame buffer to the screen
 class Renderer {
-public:
-	enum DrawMode {
-		DRAW_LINE,
-		DRAW_TRIANGLE
-	};
+   public:
+    enum DrawMode {
+        DRAW_LINE,
+        DRAW_TRIANGLE
+    };
 
+    Renderer(const int width, const int height);
+    void RunExample();
+    void SetShader(VertexShader *vertex_shader, FragmentShader *fragment_shader);
+    void Draw(const DrawMode mode);  // draw one frame
+    void DrawObject(Mesh &mesh, VertexShader &vertex_shader, FragmentShader &fragment_shader, const Rasterizer::DrawTriangleMode tri_mode = Rasterizer::TRIANGLE_LINE, const DrawMode draw_mode = DRAW_TRIANGLE);
+    void SetPolygonMode(const Rasterizer::DrawTriangleMode mode);
+    void ResetBuffer();
+    void Clear();
+    ~Renderer();
 
-	Renderer(const int width, const int height);
-	void Run();
-	void SetShader(VertexShader *vertex_shader, FragmentShader *fragment_shader);
-	void Draw(const DrawMode mode); // draw one frame
-	void DrawObject(Mesh &mesh, VertexShader &vertex_shader, FragmentShader &fragment_shader, const Rasterizer::DrawTriangleMode tri_mode = Rasterizer::TRIANGLE_LINE, const DrawMode draw_mode = DRAW_TRIANGLE);
-	void SetPolygonMode(const Rasterizer::DrawTriangleMode mode);
-	void ResetBuffer();
-	void Clear();
-	~Renderer();
+   private:
+    void SetPixel(const int x, const int y, const vec4 &color);
+    void SetDepth(const int x, const int y, const float z);
+    void SetFrame();
+    void SetUI();
+    void DrawFrame();
+    void Input();
+    bool Press();
 
+    void LoadCoordinateAxis();
+    void DrawCoordinateAxis();
 
+    unsigned int width_, height_, screen_size_;
 
-private:
+    // frame setting
+    time_point<steady_clock> last_time_;
+    double delta_time_;
+    float fps_;
+    unsigned int frame_count_;
 
-	void SetPixel(const int x, const int y, const vec4 &color);
-	void SetDepth(const int x, const int y, const float z);
-	void SetFrame();
-	void SetUI();
-	void DrawFrame();
-	void Input();
-	bool Press();
+    // pipeline stages
+    VertexShader *vertex_shader_;
+    FragmentShader *fragment_shader_;
 
-	void LoadCoordinateAxis();
-	void DrawCoordinateAxis();
+    PrimitiveAssembler primitve_assembler_;
+    Rasterizer rasterizer_;
+    PerSampleProcessor per_sample_proccessor_;
+    Device device_;
 
-	unsigned int width_, height_, screen_size_;
+    Camera camera_;
 
-	// frame setting
-	time_point<steady_clock> last_time_;
-	double delta_time_;
-	float fps_;
-	unsigned int frame_count_;
-	
-	// pipeline stages
-	VertexShader *vertex_shader_;
-	FragmentShader *fragment_shader_;
+    // all buffers
+    std::vector<Vertex> vertex_buffer_;
+    std::vector<uint32_t> element_buffer_;
+    std::vector<VertexOut> vertex_out_buffer_;
+    std::vector<Fragment> fragment_buffer_;
+    util::Array<unsigned char> frame_buffer_;
+    util::Array<float> depth_buffer_;
 
-	PrimitiveAssembler primitve_assembler_;
-	Rasterizer rasterizer_;
-	PerSampleProcessor per_sample_proccessor_;
-	Device device_;
+    Rasterizer::DrawTriangleMode polygon_mode_;
 
-	Camera camera_;
+    // coordinate axis
+    Mesh axis_lines_[3], grid_line_x_, grid_line_y_;
 
-	// all buffers
-	std::vector<Vertex> vertex_buffer_;
-	std::vector<uint32_t> element_buffer_;
-	std::vector<VertexOut> vertex_out_buffer_;
-	std::vector<Fragment> fragment_buffer_;
-	util::Array<unsigned char> frame_buffer_;
-	util::Array<float> depth_buffer_;
-
-	Rasterizer::DrawTriangleMode polygon_mode_;
-
-
-	// coordinate axis
-	Mesh axis_lines_[3], grid_line_x_, grid_line_y_;
-
-
-	// input handling
-	std::vector<InputUnit*> inputs_;
-	float input_index_;
-	const float kPressValue = 0.18;
-	
+    // input handling
+    std::vector<InputUnit *> inputs_;
+    float input_index_;
+    const float kPressValue = 0.18;
 };
 
+}  // namespace softrd
 
-} // namespace softrd
-
-
-#endif // SOFTRD_RENDERER_H_
+#endif  // SOFTRD_RENDERER_H_
