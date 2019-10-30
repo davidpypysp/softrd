17371e92907829995cc3d2b13fd985269f28933c
Jiyu Peng <jpeng@autox.ai>
2019-10-30 15:45:58 -0700

    cmake works

--- core/src/interface/renderer_api.cc
+++ core/src/interface/renderer_api.cc
@@ -36,11 +36,9 @@ RendererAPI::RendererAPI() {
 void RendererAPI::ExamplePrint() {
   std::cout << "I'm suvived!" << std::endl;
   std::cout << "node-gyp work!!!" << std::endl;
+  Texture *specular_texture = new Texture("resource/container2_specular.png");
 
-  // Texture *specular_texture = new Texture(
-  //     "resource/"
-  //     "container2_"
-  //     "specular.png");
+  Model *example_model = new Model();
 }
 
 RendererAPI::~RendererAPI() {}
