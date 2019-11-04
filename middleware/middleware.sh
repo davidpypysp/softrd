ee50756a59ec24b028908662af273d71169b7525
davidpy <davidpy1992@gmail.com>
2019-11-04 10:48:41 -0800

    update script rebuild

--- middleware/middleware.sh
+++ middleware/middleware.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+script_path=$PWD
+
 function build_core() {
     if [ ! -d "$PWD/build-core" ]; then
         mkdir "$PWD/build-core"
@@ -10,9 +12,18 @@ function build_core() {
     make -j8
 }
 
+function build_node() {
+
+    node-gyp rebuild
+
+}
+
 function rebuild() {
     rm -rf "$PWD/build-core"
     build_core
+
+    cd $script_path
+    build_node
 }
 
 function main() {
