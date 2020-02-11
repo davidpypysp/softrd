11bd32f4d3a2bbc46d2ed933f33510736039b00a
davidpy <davidpy1992@gmail.com>
2020-02-10 23:30:55 -0800

    gdb & core.sh

---
+++ core/core.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+script_path=$PWD
+
+function build() {
+    if [ ! -d "$script_path/build-cmake" ]; then
+        mkdir "$script_path/build-cmake"
+    fi
+
+    cd "$script_path/build-cmake"
+    echo 'build_cmake in ' $PWD
+
+    cmake "$script_path"
+    make -j8
+}
+
+function main() {
+    local cmd=$1
+
+    case $cmd in
+    build)
+        build $@
+        ;;
+    *) ;;
+
+    esac
+}
+
+main $@
