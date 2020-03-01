e5a57f1736f50341d113e79356d61094eacf09e6
a <davidpy1992@gmail.com>
2020-03-01 09:31:36 -0800

    fix shell & binding.gyp

--- core/core.sh
+++ core/core.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-script_path=$(dirname $(readlink -f "$0"))
+if [ "$(uname)" == "Darwin" ]; then
+    script_path=$(dirname $(realpath "$0"))
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then 
+    script_path=$(dirname $(readlink -f "$0"))
+fi
+
+echo "script_path = " $script_path
 
 function build() {
     if [ ! -d "$script_path/build-cmake" ]; then
