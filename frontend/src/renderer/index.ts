e981fecb6ab5be29c17900c1161be0639edc614d
davidpy <davidpy1992@gmail.com>
2020-03-26 10:04:40 -0700

    fps text

--- frontend/src/renderer/index.ts
+++ frontend/src/renderer/index.ts
@@ -1,19 +1,22 @@
 const { softrd } = window as any;
 
 class Renderer {
-    public canvasElement: any = null;
-    public context2D: any = null;
-    public imageData: any = null;
-    public softrdAddon: any = null;
+    public canvasElement = null;
+    public context2D = null;
+    public imageData = null;
+    public softrdAddon = null;
+    public lastExcutedTime: number = 0;
+    public fps: number = 0;
 
-    init(canvasId: any) {
+    init(canvasId) {
         this.canvasElement = document.getElementById(canvasId);
         this.context2D = this.canvasElement.getContext("2d");
         this.imageData = this.context2D.getImageData(0, 0,
             this.canvasElement.width, this.canvasElement.height);
 
         this.softrdAddon = new softrd.RendererAPIAddon(10);
-
+        this.lastExcutedTime = performance.now();
+        this.drawFrameLoop();
     }
 
     draw() {
@@ -35,6 +38,15 @@ class Renderer {
 
     reDraw() {
         this.context2D.putImageData(this.imageData, 0, 0);
+    }
+
+    drawFrameLoop() {
+        setInterval(() => {
+            this.drawFrame();
+            const currentTime = performance.now();
+            this.fps = 1000.0 / (currentTime - this.lastExcutedTime)
+            this.lastExcutedTime = currentTime;
+        }, 0);
     }
 }
 
