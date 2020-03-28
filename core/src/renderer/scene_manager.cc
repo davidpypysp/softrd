e91a843919a8fd1b4eed455b933dfbc586ee3e76
davidpy <davidpy1992@gmail.com>
2020-03-28 11:36:17 -0700

    extend functions

--- core/src/renderer/scene_manager.cc
+++ core/src/renderer/scene_manager.cc
@@ -2,7 +2,21 @@
 
 namespace softrd {
 
-SceneManager::SceneManager() {}
+SceneManager::SceneManager() {
+  this->set_camera(std::make_shared<Camera>(float(640.0) / float(480.0)));
+  this->InitShaders();
+}
+
+void SceneManager::InitShaders() {
+  // vertex shaders
+  auto vertex_shader_light = std::make_shared<VertexShaderLight>();
+  vertex_shaders_.emplace(
+      std::make_pair("vertex_shader_light", vertex_shader_light));
+
+  // fragment shaders
+  auto fragment_shader = std::make_shared<FragmentShader>();
+  fragment_shaders_.emplace(std::make_pair("fragment_shader", fragment_shader));
+}
 
 std::shared_ptr<SceneObject> SceneManager::AddSceneObject(
     const std::string &id) {
@@ -12,6 +26,26 @@ std::shared_ptr<SceneObject> SceneManager::AddSceneObject(
 
   auto scene_object = std::make_shared<SceneObject>();
   scene_object->set_id(id);
+
+  auto mesh = std::make_shared<Mesh>();
+  mesh->LoadCube2();
+  scene_object->set_mesh(mesh);
+
+  auto vertex_shader_iterator = vertex_shaders_.find("vertex_shader_light");
+  if (vertex_shader_iterator != vertex_shaders_.end()) {
+    auto vertex_shader_light = vertex_shader_iterator->second;
+    scene_object->set_vertex_shader(vertex_shader_light);
+  }
+
+  auto fragment_shader_iterator = fragment_shaders_.find("fragment_shader");
+  if (fragment_shader_iterator != fragment_shaders_.end()) {
+    auto fragment_shader = fragment_shader_iterator->second;
+    scene_object->set_fragment_shader(fragment_shader);
+  }
+
+  auto material = std::make_shared<Material>(
+      vec3(1.0, 1.0, 1.0), vec3(1.0, 0.5, 0.31), vec3(0.5, 0.5, 0.5), 32.0);
+  scene_object->set_material(material);
 
   scene_objects_.emplace(std::make_pair(id, scene_object));
 
--- core/src/renderer/scene_manager.h
+++ core/src/renderer/scene_manager.h
@@ -3,6 +3,7 @@
 
 #include <unordered_map>
 
+#include "src/components/camera.h"
 #include "src/renderer/scene_object.h"
 
 namespace softrd {
@@ -10,11 +11,23 @@ class SceneManager {
  public:
   SceneManager();
 
+  void InitShaders();
+
   std::shared_ptr<SceneObject> AddSceneObject(const std::string &id);
   std::shared_ptr<SceneObject> GetSceneObject(const std::string &id);
 
+  void set_camera(const std::shared_ptr<Camera> &camera) { camera_ = camera; }
+  std::shared_ptr<Camera> camera() const { return camera_; }
+
  private:
   std::unordered_map<std::string, std::shared_ptr<SceneObject>> scene_objects_;
+
+  std::unordered_map<std::string, std::shared_ptr<VertexShader>>
+      vertex_shaders_;
+  std::unordered_map<std::string, std::shared_ptr<FragmentShader>>
+      fragment_shaders_;
+
+  std::shared_ptr<Camera> camera_;
 };
 
 }  // namespace softrd
