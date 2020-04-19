14d33823277ae7b55ce94d6079f259d7a48ee1b5
davidpy <davidpy1992@gmail.com>
2020-04-19 08:21:56 -0700

    assimp submodule

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -1,17 +1,12 @@
-find_library(SOIL_LIB SOIL)
-message("soil found: " ${SOIL_LIB})
-
-find_library(ASSIMP_LIB assimp)
-message("assimp found: " ${ASSIMP_LIB})
-
+# find_library(SOIL_LIB SOIL)
+# find_library(ASSIMP_LIB assimp)
 find_library(SDL2_LIB SDL2)
-message("sdl2 found: " ${SDL2_LIB}) 
+find_library(SD2_LTTF_LIB SDL2_ttf)
 
-find_library(SDL2_TTF_LIB SDL2_ttf)
-message("sdl2_ttf found: " ${SDL2_TTF_LIB})
-
+# google libs
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/googletest)
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/abseil-cpp)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/soil)
+add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/assimp)
 
