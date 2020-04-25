b1b22e06dfc03d5988ae992ab42bb594e51f6580
davidpy <davidpy1992@gmail.com>
2020-04-25 11:03:23 -0700

    fix cmakelist errors

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -1,7 +1,8 @@
 # find_library(SOIL_LIB SOIL)
 # find_library(ASSIMP_LIB assimp)
 find_library(SDL2_LIB SDL2)
-find_library(SD2_LTTF_LIB SDL2_ttf)
+find_library(SDL2_TTF_LIB SDL2_ttf)
+
 
 # google libs
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/googletest)
