bd204802e22683a2a9ff950ba9ba0a49a050ebf9
davidpy <davidpy1992@gmail.com>
2020-04-26 10:47:45 -0700

    fix cmake

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -5,7 +5,6 @@ find_library(SDL2_TTF_LIB SDL2_ttf)
 
 
 # google libs
-# add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/googletest)
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/abseil-cpp)
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/glog)
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/gflags)
