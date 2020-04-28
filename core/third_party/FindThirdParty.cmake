2636d7f8ee5cece243e9acdd6b2170328928ddd9
davidpy <davidpy1992@gmail.com>
2020-04-28 10:40:11 -0700

    clean deperecated code

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -1,8 +1,3 @@
-# find_library(SOIL_LIB SOIL)
-# find_library(ASSIMP_LIB assimp)
-find_library(SDL2_LIB SDL2)
-find_library(SDL2_TTF_LIB SDL2_ttf)
-
 
 # google libs
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/abseil-cpp)
