b9ee3b011cfe4f0fe9efcb0e40236b4aa5fc0ff0
davidpy <davidpy1992@gmail.com>
2020-04-29 13:52:27 -0700

    refine cmakelists

--- core/FindSoftrdCore.cmake
+++ core/FindSoftrdCore.cmake
@@ -5,7 +5,7 @@ include_directories(${CMAKE_CURRENT_LIST_DIR}/third_party/soil/inc)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/third_party/assimp/include)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/build/third_party/assimp/include)
 
-add_library(renderer_api SHARED IMPORTED )
-set_target_properties(renderer_api PROPERTIES 
+add_library(core_renderer_api SHARED IMPORTED )
+set_target_properties(core_renderer_api PROPERTIES 
     IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/build/src/interface/libSoftrdAPI.so
 )
