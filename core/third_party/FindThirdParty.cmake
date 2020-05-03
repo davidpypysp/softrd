00d4d3b1a8949de43b775249ba5d27c0c2915b2c
dv <davidpy1992@gmail.com>
2020-05-03 12:58:14 -0700

    remove gflag folder

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -6,7 +6,7 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/abseil-cpp)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/glog)
 
-add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/gflags)
+# add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/gflags)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/soil)
 
