2e072ee03b22428e64d395d3d2a454826ba73aeb
dv <davidpy1992@gmail.com>
2020-05-04 10:10:44 -0700

    win can build all!

--- core/third_party/FindThirdParty.cmake
+++ core/third_party/FindThirdParty.cmake
@@ -6,8 +6,6 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/abseil-cpp)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/glog)
 
-# add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/gflags)
-
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/soil)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/assimp)
