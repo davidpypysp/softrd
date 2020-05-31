6372a230c0bc931410afbcfdad62c0b2011dce0d
dv <dv@pop-os.localdomain>
2020-05-30 19:00:08 -0700

    rename

--- core/third_party/FindThirdPartyWASM.cmake
+++ core/third_party/FindThirdPartyWASM.cmake
@@ -1,4 +1,4 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/soil/include)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/assimp/include)
 
-set(soil_lib ${CMAKE_SOURCE_DIR}/third_party/wasm/soil/lib/libSOIL.bc)
+set(SOIL_LIB ${CMAKE_SOURCE_DIR}/third_party/wasm/soil/lib/libSOIL.bc)
