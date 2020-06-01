baf0ce306975ee11e600be93f8ab656f4ee0111a
dv <dv@pop-os.localdomain>
2020-06-01 00:32:19 -0700

    import gtest

--- core/src/common/texture_test.cc
+++ core/src/common/texture_test.cc
@@ -1,6 +1,5 @@
 #include "src/common/texture.h"
 
-#include <glog/logging.h>
 #include <gtest/gtest.h>
 
 using namespace softrd;
@@ -8,9 +7,9 @@ using namespace softrd;
 TEST(CameraTest, Genral) { Texture texture("../../../resource/container.jpg"); }
 
 int main(int argc, char** argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  // gflags::ParseCommandLineFlags(&argc, &argv, true);
   testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging(argv[0]);
+  // google::InitGoogleLogging(argv[0]);
 
   return RUN_ALL_TESTS();
 }
--- core/third_party/FindThirdPartyWASM.cmake
+++ core/third_party/FindThirdPartyWASM.cmake
@@ -1,4 +1,10 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/soil/include)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/assimp/include)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/googletest/include)
 
 set(SOIL_LIB ${CMAKE_SOURCE_DIR}/third_party/wasm/soil/lib/libSOIL.bc)
+
+set(GTest_DIR ${CMAKE_CURRENT_LIST_DIR}/wasm/googletest/lib/cmake/GTest)
+find_package(GTest CONFIG REQUIRED
+    PATHS ${CMAKE_CURRENT_LIST_DIR}/wasm/googletest/lib/cmake/GTest
+)
