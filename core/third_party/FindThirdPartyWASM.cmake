e54c5489866f685f18234176eda083d2b93f8b79
dv <dv@pop-os.localdomain>
2020-06-01 11:36:02 -0700

    fix cmake

--- core/third_party/FindThirdPartyWASM.cmake
+++ core/third_party/FindThirdPartyWASM.cmake
@@ -1,6 +1,5 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/soil/include)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/assimp/include)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/wasm/googletest/include)
 
 set(SOIL_LIB ${CMAKE_SOURCE_DIR}/third_party/wasm/soil/lib/libSOIL.bc)
 
