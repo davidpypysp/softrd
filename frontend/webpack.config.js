92e0b39649d62d1f5c997eb72aed16478e311ee8
davidpysp <davidpy1992@gmail.com>
2020-06-19 19:11:22 -0700

    eslint fix

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|jsx|ts|tsx)$/,
                 exclude: [
                     /node_modules/
                 ],
