8e0afe1082c8ca75dd5d4239f9632fc6f1e095f9
dv <dv@pop-os.localdomain>
2020-07-03 12:55:48 -0700

    gui jss

--- frontend/src/ui/GUI.tsx
+++ frontend/src/ui/GUI.tsx
@@ -9,17 +9,28 @@ import MainView from "src/ui/Layouts/MainView";
 import LeftPanel from "src/ui/Layouts/LeftPanel";
 import RightPanel from "src/ui/Layouts/RightPanel";
 
-export class GUI extends React.Component {
-    render() {
-        return (
-            <Card className="bp3-dark gui">
-                <Header />
-                <div className="container">
-                    <LeftPanel />
-                    <MainView />
-                    <RightPanel />
-                </div>
-            </Card>
-        );
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    gui: {
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        width: "100%",
+        padding: 0
     }
+});
+
+export default function GUI(props) {
+    const classes = useStyles();
+    return (
+        <Card className={`bp3-dark ${classes.gui}`}>
+            <Header />
+            <div className="container">
+                <LeftPanel />
+                <MainView />
+                <RightPanel />
+            </div>
+        </Card>
+    );
 }
