6a4c63ce485f4125fbc3ef69bdcb16792582ccfa
dv <dv@pop-os.localdomain>
2020-07-03 12:30:30 -0700

    jss

--- Build.md
+++ Build.md
@@ -2,6 +2,9 @@
 ```
 # In repo root dir
 
+# docker-compose start dev container
+docker-compose run softrd-dev bash
+
 # build dev image
 docker build -t davidpy1992/softrd-dev -f docker/softrd-dev.dockerfile .
 
--- frontend/src/ui/Layouts/Header.tsx
+++ frontend/src/ui/Layouts/Header.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Card } from "@blueprintjs/core";
 
-export default class Header extends React.Component {
-    render() {
-        return <Card className="header">Help</Card>;
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    header: {
+        height: 35,
+        padding: 5
     }
+});
+
+export default function Header(props) {
+    const classes = useStyles();
+    return <Card className={classes.header}>Help</Card>;
 }
