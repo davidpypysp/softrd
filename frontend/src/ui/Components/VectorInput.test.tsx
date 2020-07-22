1f12f548f78e3e48aa60ec1b2779fd3a99b34a88
davidpypysp <dv@pop-os.localdomain>
2020-07-22 13:20:01 -0700

    vectorinput test

--- frontend/src/ui/Components/VectorInput.test.tsx
+++ frontend/src/ui/Components/VectorInput.test.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { HTMLAttributes, shallow, ShallowWrapper } from "enzyme";
-import Header from "./Header";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { NumericInput } from "@blueprintjs/core";
 
-import { Vec3Input } from "./Vec3Input";
+import { Vec3Input } from "./VectorInput";
 
 configure({ adapter: new Adapter() });
 
 let child: ShallowWrapper<undefined, undefined> = null;
-beforeEach(() => (child = shallow(<Header />)));
+beforeEach(() => (child = shallow(<Vec3Input data={{ x: 1, y: 2, z: 3 }} />)));
 
 // checking that all is fine and component has been rendered
 it("should render without error", () => expect(child.length).toBe(1));
 
 it("should render one Card component", () => {
-    expect(child.find(Card)).toHaveLength(1);
-}
+    expect(child.find(NumericInput)).toHaveLength(3);
+});
